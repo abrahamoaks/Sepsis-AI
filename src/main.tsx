36367ac9b44b2e7f87301c003ada37e257f55b43
Abraham Obianke <cruzcontactdesk@gmail.com>
2026-09-20 00:01:58 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { Dashboard } from './components/Dashboard';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+export default function App() {
+  return <Dashboard />;
+}
